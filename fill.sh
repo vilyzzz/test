a69913b5c682260e0b67a696368671c7319922f4
vilius <vilius.al@gmail.com>
2016-01-29 10:27:38 +0000

    .gitignore updates with  *swp insert to log table completed

--- fill.sh
+++ fill.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-[[ -z ${1} ]] && count=400 || count=${1}
-
-. .config.priv
+. config.priv
 
 while true
 do 
-	for n  in `seq 1 3 9`
-	do
-		mysql -h ${HOSTPREFIX}150 -u ${USER} -p${PASS} test -e "update ttbl set count=$[$count*10+${n}] where id=${n};"
-		mysql -h ${HOSTPREFIX}151 -u ${USER} -p${PASS} test -e "update ttbl set count=$[$count*10+${n}+1] where id=$[${n}+1];"
-		mysql -h ${HOSTPREFIX}152 -u ${USER} -p${PASS} test -e "update ttbl set count=$[$count*10+${n}+2] where id=$[${n}+2];"
-        mysql -h ${HOSTPREFIX}150 -u ${USER} -p${PASS} test -e "insert into ttbl (id, count) values (${n},$[$count*10+${n}]);"
-        mysql -h ${HOSTPREFIX}151 -u ${USER} -p${PASS} test -e "insert into ttbl (id, count) values ($[${n}+1],$[$count*10+${n}+1]);"
-        mysql -h ${HOSTPREFIX}152 -u ${USER} -p${PASS} test -e "insert into ttbl (id, count) values ($[${n}+2],$[$count*10+${n}+2]);"
 
-		mysql -h ${HOSTPREFIX}150 -u ${USER} -p${PASS} test -e "update ttbl2 set string=cast($[$count*10+${n}] AS char) where id=${n};"
-		mysql -h ${HOSTPREFIX}151 -u ${USER} -p${PASS} test -e "update ttbl2 set string=cast($[$count*10+${n}+1] AS char) where id=$[${n}+1];"
-		mysql -h ${HOSTPREFIX}152 -u ${USER} -p${PASS} test -e "update ttbl2 set string=cast($[$count*10+${n}+2] AS char) where id=$[${n}+2];"
-		mysql -h ${HOSTPREFIX}152 -u ${USER} -p${PASS} test -e "select * FROM ttbl LEFT JOIN (ttbl2) ON (ttbl.id = ttbl2.id);"
-	done
-	count=$[$count+1]
-	[[ $((${count} % 20 )) -eq 0 ]] && echo sleeping... && sleep 60
+	SRV=$(( RANDOM % 3 ))
+	NR=$(( RANDOM % 9 ))
+	NUMBER=$RANDOM
+
+	mysql -h "${HOSTPREFIX}15${SRV}" -u ${USER} -p${PASS} ${DB} -e "update numbers set number=${NUMBER} where id=${NR};"
+	mysql -h "${HOSTPREFIX}15${SRV}" -u ${USER} -p${PASS} ${DB} -e "update strings set number=cast(${NUMBER} AS char) where id=${NR};"
+	mysql -h "${HOSTPREFIX}15${SRV}" -u ${USER} -p${PASS} ${DB} -e "update stats set number=number+1 where id='all';"
+	if [[ $[${NR}%2] -eq 0 ]]
+	then 
+		mysql -h "${HOSTPREFIX}15${SRV}" -u ${USER} -p${PASS} ${DB} -e "update stats set number=number+1 where id='even';"
+	fi
+	if [[ $[${NR}%2] -eq 1 ]]
+	then 
+		mysql -h "${HOSTPREFIX}15${SRV}" -u ${USER} -p${PASS} ${DB} -e "update stats set number=number+1 where id='odd';"
+	fi
+
+	mysql -h "${HOSTPREFIX}15${SRV}" -u ${USER} -p${PASS} ${DB} -e "insert into log (id, server, pos, number) values ( ((SELECT nid FROM (SELECT max(id)+1 AS nid FROM log ) AS x)), '${HOSTPREFIX}15${SRV}','${NR}','${NUMBER}');"
+
 done
